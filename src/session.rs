28898bdbc0a841797f92e220b22994414df06dea
Zack Slayton <zack@xtify.com>
2014-08-02 17:09:22 -0400

    Added send_bytes_with_receipt method to session.

--- src/session.rs
+++ src/session.rs
@@ -11,6 +11,7 @@ use subscription::Auto;
 use subscription::AckOrNack;
 use subscription::Client;
 use subscription::ClientIndividual;
+use headers::Header;
 use headers::Subscription;
 use headers::Ack;
 use headers::Id;
@@ -20,6 +21,7 @@ use transaction::Transaction;
 pub struct Session {
   next_transaction_id: uint,
   next_subscription_id: uint,
+  next_receipt_id: uint,
   subscriptions: HashMap<String, ::subscription::Subscription>,
   pub connection : Connection,
   error_callback: fn(Frame) -> ()
@@ -30,6 +32,7 @@ impl Session {
     Session {
       next_transaction_id: 0,
       next_subscription_id: 0,
+      next_receipt_id: 0,
       subscriptions: HashMap::with_capacity(1),
       connection: conn,
       error_callback: Session::default_error_callback
@@ -56,12 +59,27 @@ impl Session {
     id
   }
 
+  fn generate_receipt_id(&mut self) -> uint {
+    let id = self.next_receipt_id;
+    self.next_subscription_id += 1;
+    id
+  }
+
   pub fn send_text(&mut self, topic: &str, body: &str) -> IoResult<()> {
     Ok(try!(self.send_bytes(topic, "text/plain", body.as_bytes())))
   }
  
   pub fn send_bytes(&mut self, topic: &str, mime_type: &str, body: &[u8]) -> IoResult<()> {
     let send_frame = Frame::send(topic, mime_type, body);
+    Ok(try!(send_frame.write(&mut self.connection.writer)))
+  }
+
+  pub fn send_bytes_with_receipt(&mut self, topic: &str, mime_type: &str, body: &[u8]) -> IoResult<()> {
+    let mut send_frame = Frame::send(topic, mime_type, body);
+    let receipt_id = format!("message/{}", self.generate_receipt_id().to_string());
+    send_frame.headers.push(
+      Header::from_key_value("receipt", receipt_id.as_slice())
+    );
     Ok(try!(send_frame.write(&mut self.connection.writer)))
   }
 
@@ -98,6 +116,10 @@ impl Session {
     // Check for ERROR frame
     match frame.command.as_slice() {
        "ERROR" => return (self.error_callback)(frame),
+       "RECEIPT" => {
+          debug!("RECEIPT received:\n{}", frame);
+          return;
+        }
         _ => {} // No operation
     };
 
