56fd02b9da506750b704cc821c254c4bb0d81af7
Zack Slayton <zack.slayton@gmail.com>
2015-03-29 22:18:27 -0400

    Toying with mio for timeouts.

--- src/stomp.rs
+++ src/stomp.rs
@@ -12,10 +12,49 @@
 extern crate log;
 extern crate collections;
 
+extern crate mio;
+
+use mio::*;
+
 use session_builder::SessionBuilder;
 
 pub fn session<'a>(host: &'a str, port: u16) -> SessionBuilder<'a>{
   SessionBuilder::new(host, port)
+}
+
+enum StompTimeout {
+  SendHeartBeat,
+  ReceiveHeartBeat
+}
+
+struct StompHandler;
+
+const SEND_HEART_BEAT_MS: u64 = 2_000;
+const RECEIVE_HEART_BEAT_MS: u64 = 6_000;
+
+impl Handler for StompHandler{
+  type Timeout = StompTimeout;
+  type Message = ();
+
+  fn timeout(&mut self, event_loop: &mut EventLoop<StompHandler>, mut timeout: StompTimeout) {
+    match timeout {
+      StompTimeout::SendHeartBeat => {
+        println!("Send heartbeat");
+        event_loop.timeout_ms(StompTimeout::SendHeartBeat, ::SEND_HEART_BEAT_MS);
+      },
+      StompTimeout::ReceiveHeartBeat => {
+        println!("Receive heartbeat");
+        event_loop.timeout_ms(StompTimeout::ReceiveHeartBeat, ::RECEIVE_HEART_BEAT_MS);
+      },
+    }
+  }
+}
+
+pub fn event_loop() {
+  let mut event_loop = EventLoop::new().unwrap();
+  let timeout = event_loop.timeout_ms(StompTimeout::SendHeartBeat, 2000).unwrap();
+  let timeout = event_loop.timeout_ms(StompTimeout::ReceiveHeartBeat, 6000).unwrap();
+  let _ = event_loop.run(&mut StompHandler);
 }
 
 pub mod connection;
